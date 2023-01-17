483bcce580ea61ee0f298344ebc601d1af023bd2
KonarzewskiP <pawelkonarzewski13@gmail.com>
2023-01-17 18:21:09 +0000

    ignore

--- src/assets/variables.js
+++ src/assets/variables.js
@@ -1,5 +1,4 @@
 // Colors
-
 export const gray = "#d4d2d5";
 export const yellow = "#ffe066";
 export const blue = "#2b59c3";
@@ -8,9 +7,7 @@ export const pink = "#ffeaee";
 export const purple = "#673ab7";
 export const black = "#272932";
 export const white = "#fcfcfc";
-
 // Type
-
 export const fontHeading = `"Montserrat", sans-serif`; // 700, 800
 export const fontBody = `"Lora", serif`; // 400, 700, italic
 export const fontCode = `"Fira Code", monospace`; // 400
